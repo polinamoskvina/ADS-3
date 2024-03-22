9fe534abfbe9d36c5fdc9eb0a1fa46f6bf0c0b81
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-03-22 12:32:06 +0300

    B123.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,33 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                count++;
+                i--;
+            }
+            i = mid + 1;
+            while (i < size && arr[i] == value) {
+                count++;
+                i++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (count == 0) {
+        return 0;
+    }
+    return count;
 }
